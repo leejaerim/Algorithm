06e99c9fbdbc8dea03b8392f4362de84c664307d
leejaerim <ger6608@naver.com>
2022-06-20 21:21:50 +0900

    Vaild Parenthesese

---
+++ leetcode/20. Valid Parentheses.py
@@ -0,0 +1,18 @@
+brackets = {"{":"}","(":")","[":"]"}
+ch = ""
+s= "{}"
+for i in s:
+    print(s.count(brackets[i]))
+# if s.count(brackets[i]) <= 0 and not s.count(brackets[i]) == s.count(i):
+#     return False
+# else:
+#     print(s[s.index(i):s.index(brackets[i])])
+# if isValid(s[s.index(i):s.index(brackets[i])]):
+#     return True
+# else:
+#     return False
+                
+                
+                
+            
+            
