134918f62684bd54d6e5c7d581ce1687843d75ab
leejaerim <ger6608@naver.com>
2023-03-21 17:56:31 +0900

    [프로그래머스] - 230321 Level 2. 롤케잌자르기

---
+++ Programmers/롤케이크자르기.py
@@ -0,0 +1,22 @@
+from collections import Counter
+def solution(topping):
+        ans = 0
+        first_flag = False
+        second_flag = False
+        left = Counter(topping[:0])
+        right = Counter(topping[0:])
+        for i in range(0,len(topping)):
+                left[topping[i]] += 1
+                if right[topping[i]] == 1:
+                        del right[topping[i]]
+                else :
+                  right[topping[i]] -= 1
+                if len(left) == len(right) :
+                        first_flag =True
+                        second_flag = True
+                        ans+=1
+                else :
+                     기   first_flag = False
+                if first_flag == False and second_flag == True:
+                        break
+        return ans
