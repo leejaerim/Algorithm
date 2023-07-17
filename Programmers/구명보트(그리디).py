d66830a11c0e9b5ccdf0121eeaf3fe034d9b6fed
leejaerim <ger6608@naver.com>
2023-07-17 22:20:00 +0900

    [프로그래머스] - 230717 Level 2. 구명보트

---
+++ Programmers/구명보트(그리디).py
@@ -0,0 +1,14 @@
+def solution(people:list,limit:int)->int:
+    answer = 0
+    people.sort(reverse=True)
+    start = 0
+    end = len(people)-1
+    while start <= end :
+        if people[start] + people[end] <= limit:
+            answer += 1
+            start += 1
+            end -= 1
+        else:
+            answer += 1
+            start +=1
+    return answer
